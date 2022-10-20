49ac4d5c4e1631af7194ff7d7f19b48ba74864bd
Brian Blais <bblais@gmail.com>
2022-10-20 11:24:12 -0400

    formatting

--- Introduction.py
+++ Introduction.py
@@ -5,7 +5,7 @@
 # 
 # 
 
-# In this part we will explore the clinical basis for amblyopia and its treatments.  In the -@sec-models-of-development and @sec-models-of-treatments we will explore the models that are used to describe the deficits from amblyopia and their treatment, respectively. 
+# In this part we will explore the clinical basis for amblyopia and its treatments.  In the @sec-models-of-development and @sec-models-of-treatments we will explore the models that are used to describe the deficits from amblyopia and their treatment, respectively. 
 
 # In[ ]:
 
